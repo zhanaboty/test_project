70b415ae86ff713a9a12fcb58710721ade7fc758
Zhanabek Maksotuly <zhanabekmaksotuly@MacBook-Pro-Zhanabek.local>
2023-11-20 23:47:52 +0600

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        
+        sdf
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
